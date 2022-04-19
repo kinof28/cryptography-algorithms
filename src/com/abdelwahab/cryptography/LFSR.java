f0c0344343e5c1a6a08594bb7c5b9be3b6182285
Abdelwahab <ab28fb@gmail.com>
2022-04-19 04:16:03 +0100

    LFSR Algorithm

--- src/com/abdelwahab/cryptography/LFSR.java
+++ src/com/abdelwahab/cryptography/LFSR.java
@@ -1,4 +1,73 @@
 package com.abdelwahab.cryptography;
 
 public class LFSR {
+
+    public String encrypt(String message, String key) {
+        String encryptedMessage = "";
+        String messageInBinary = this.getMessageInBinary(message);
+        System.out.println("message in binary : " + messageInBinary);
+        String finalKey = this.getKey(key, messageInBinary.length());
+        System.out.println("key : " + finalKey);
+        for (int i = 0; i < messageInBinary.length(); i++) {
+            encryptedMessage = messageInBinary.charAt(i) == finalKey.charAt(i) ? encryptedMessage + 0 : encryptedMessage + 1;
+        }
+        return encryptedMessage;
+    }
+
+    private String getMessageInBinary(String message) {
+        String messageInBinary = "";
+        for (int i = 0; i < message.length(); i++) {
+            messageInBinary += charToBinary(message.charAt(i));
+        }
+        return messageInBinary;
+    }
+
+    private String charToBinary(char c) {
+        String binary = Integer.toBinaryString(c);
+        String leadingZeros = "00000000";
+        String result = leadingZeros.substring(0, 8 - binary.length()) + binary;
+        return result;
+    }
+
+    private String getKey(String key, int length) {
+        String finalKey = key;
+        for (int i = 0; i < length - key.length(); i++) {
+            finalKey += this.getSingleKeyChar(finalKey.substring(i));
+        }
+        return finalKey;
+    }
+
+    private String getSingleKeyChar(String key) {
+        String result = "";
+        int first = Integer.parseInt(key.charAt(0) + "");
+        int third = Integer.parseInt(key.charAt(2) + "");
+        int fifth = Integer.parseInt(key.charAt(4) + "");
+        int seventh = Integer.parseInt(key.charAt(6) + "");
+        int eighth = Integer.parseInt(key.charAt(7) + "");
+        result = (first + third + fifth + seventh + eighth) % 2 == 0 ? "0" : "1";
+        return result;
+    }
+
+        private String convertBinaryToMessage(String messageInBinary){
+        String result="";
+        String part;
+        for (int i = 0; i < messageInBinary.length(); i+=8) {
+            part=messageInBinary.substring(i,i+8);
+            int c=Integer.parseInt(part,2);
+            result+=Character.toString(c);
+        }
+        return result;
+    }
+    public String decrypt(String message, String key) {
+        String decryptedMessage = "";
+        String finalKey = this.getKey(key, message.length());
+        System.out.println("key : " + finalKey);
+        for (int i = 0; i < message.length(); i++) {
+            decryptedMessage = message.charAt(i) == finalKey.charAt(i) ? decryptedMessage + 0 : decryptedMessage + 1;
+        }
+        System.out.println("decrypted message :"+this.convertBinaryToMessage(decryptedMessage));
+        return decryptedMessage;
+    }
+
+
 }
--- src/com/abdelwahab/cryptography/Main.java
+++ src/com/abdelwahab/cryptography/Main.java
@@ -6,12 +6,15 @@ public class Main {
 //        Caesar caesar=new Caesar();
 //        System.out.println(caesar.encrypt("HelloWorld",5,true));
 //        System.out.println(caesar.decrypt("mjqqtbtwqi",5,false));
-        Vigenere vigenere=new Vigenere();
-        System.out.println(vigenere.encrypt("decodex","isi",true));
-        System.out.println(vigenere.decrypt("lwkwvmf","isi",true));
-        System.out.println(vigenere.encrypt("decodex","isi",false));
-        System.out.println(vigenere.decrypt("lwkwvmf","isi",false));
-//        System.out.println(vigenere.encryptNumeric("decodex","isi"));
-//        System.out.println(vigenere.decrypt("lwkwvmf","isi"));
+        //test for Vigenere Algorithm
+//        Vigenere vigenere=new Vigenere();
+//        System.out.println(vigenere.encrypt("decodex","isi",true));
+//        System.out.println(vigenere.decrypt("lwkwvmf","isi",true));
+//        System.out.println(vigenere.encrypt("decodex","isi",false));
+//        System.out.println(vigenere.decrypt("lwkwvmf","isi",false));
+        //test for LFSR Algorithm
+        LFSR lfsr=new LFSR();
+        System.out.println("encryption : "+lfsr.encrypt("decodex","01010101"));
+        System.out.println("message in binary : "+lfsr.decrypt("00110001101011010011001101110101000101001001100001101110","01010101"));
     }
 }
